a9b2c70c3ab9295e1e14575ea410e066540ee9b3
DragonoyDark <89664941+Black-software100@users.noreply.github.com>
2022-09-20 11:54:43 -0500

    up

---
+++ app/src/main/java/com/example/pokedex/models/Pokemon.java
@@ -0,0 +1,32 @@
+package com.example.pokedex.models;
+
+public class Pokemon {
+    private  int number;
+    private  String name;
+    private  String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        String[] urlPartes = url.split("/");
+        return Integer.parseInt(urlPartes[urlPartes.length-1]);
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
